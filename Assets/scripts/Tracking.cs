8c36795d886966a4ace6af0c75c469604f5c1b05
bourgeoisree <122033950+bourgeoisree@users.noreply.github.com>
2023-02-05 16:15:10 -0500

    Adds multiple image tracking, albeit with imperfect targets

--- Assets/scripts/Tracking.cs
+++ Assets/scripts/Tracking.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Zappar;
 
 public class Tracking : MonoBehaviour
 {
+    public GameObject model = null;
+    public GameObject preview = null;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,12 +21,17 @@ public class Tracking : MonoBehaviour
         
     }
     public void OnSeen()
+
     {
+        model.SetActive(true);
+        preview.SetActive(true);
         Debug.Log("This is really high stress.");
         
     }
     public void OnNotSeen()
     {
         Debug.Log("My name is Racos and I'm chill.");
+        model.SetActive(false);
+        preview.SetActive(false);
     }
     }
